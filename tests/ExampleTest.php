16a54578952b3df2c5e5411bc017dd37e1ca17c2
Filip Mikovcak <fmikovcak@gmail.com>
2022-02-06 22:33:34 +0100

    Add support for PHPUnit tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vendor\Package;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
